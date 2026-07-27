0624b54874424bf8a4929943c8c4606d5636711a
mtavaresj <154026899+mtavaresj@users.noreply.github.com>
2026-07-26 21:55:30 -0300

    Update main.cpp
    Define nome da janela como SuporteCRA

--- flutter/windows/runner/main.cpp
+++ flutter/windows/runner/main.cpp
@@ -80,7 +80,8 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
         command_line_arguments.end());
   }
 
-  std::wstring app_name = L"RustDesk";
+  std::wstring app_name = L"SuporteCRA";
+  /*
   FUNC_RUSTDESK_GET_APP_NAME get_rustdesk_app_name = (FUNC_RUSTDESK_GET_APP_NAME)GetProcAddress(hInstance, "get_rustdesk_app_name");
   if (get_rustdesk_app_name) {
     wchar_t app_name_buffer[512] = {0};
@@ -88,7 +89,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
       app_name = std::wstring(app_name_buffer);
     }
   }
-
+  */
   // Uri links dispatch
   HWND hwnd = ::FindWindowW(getWindowClassName(), app_name.c_str());
   if (hwnd != NULL) {
